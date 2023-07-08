789eb2a61a26b48f851412064b4c5a5c605b3193
Rudolph Almeida <rudolf1.almeida@gmail.com>
2023-07-08 10:56:01 +1000

    Add Subreadit model

--- readit_api/models.py
+++ readit_api/models.py
@@ -1,3 +1,44 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def str_username(self):
+    return f"u/{self.username}"
+
+
+# Override the inbuilt `User`s `__str__` to print with a leading "u/"
+User.add_to_class("__str__", str_username)
+
+
+class Subreadit(models.Model):
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )
+
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="created",
+    )
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="owns",
+    )
+
+    subscribers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="subscribes",
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"r/{self.name}"
